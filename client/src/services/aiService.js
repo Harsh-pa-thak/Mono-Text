eeb63f6fe63d77cdc05db8f81fad6347fce2b1c2
Harsh Pathak <harphy45@gmail.com>
2026-03-30 01:50:51 +0530

    ai feature

--- client/src/services/aiService.js
+++ client/src/services/aiService.js
@@ -2,3 +2,6 @@ import api from './api';
 
 export const summarize = (content) =>
   api.post('/ai/summarize', { content }).then((r) => r.data);
+
+export const sendChatMessage = (message, history, blogContent) =>
+  api.post('/ai/chat', { message, history, blogContent }).then((r) => r.data);
